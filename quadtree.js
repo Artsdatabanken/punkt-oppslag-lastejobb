adb8d812ec55f86ddb54d878def36ad905e316d4
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-07-23 10:04:19 +0200

    Point query

--- quadtree.js
+++ quadtree.js
@@ -9,74 +9,26 @@ function getChild(tree, x, y) {
   return tree[key];
 }
 
-let max = 0;
-
-function find2(quad, x, y, z) {
-  const best = { distSquared: 9e19 };
-  find2_(quad, best, x, y, z);
-  best.dist = Math.sqrt(best.distSquared) * Math.pow(0.5, z);
-  delete best.distSquared;
-  return best;
+function accumulateHits(acc, node, z) {
+  if (!node.value) return;
+  acc[z] = acc[z] || { area: 0, value: 0 };
+  const s = acc[z];
+  s.value = (s.value ? s.value / s.area : 0) + node.value / node.area;
+  s.area += node.area;
+  s.area *= s.area;
 }
 
-function distanceFromQuadSquared(px, py) {
-  const x = 0.5;
-  const y = 0.5;
-  const width = 1;
-  const height = 1;
-  dx = Math.max(Math.abs(px - x) - width / 2, 0);
-  dy = Math.max(Math.abs(py - y) - height / 2, 0);
-  return dx * dx + dy * dy;
-}
-
-function addCandidate(prio, tile, x, y, dx, dy) {
-  if (!tile) return;
-  prio.push({
-    t: tile,
-    x: x - dx,
-    y: y - dy,
-    d: 0
-  });
-}
-
-function getCandidates(quad, x, y) {
-  const prio = [];
-  addCandidate(prio, quad.nw, x, y, 0, 0);
-  addCandidate(prio, quad.ne, x, y, 0.5, 0);
-  addCandidate(prio, quad.sw, x, y, 0, 0.5);
-  addCandidate(prio, quad.se, x, y, 0.5, 0.5);
-  for (let i = 0; i < prio.length; i++)
-    prio[i].d = distanceFromQuadSquared(prio[i].x, prio[i].y);
-  prio.sort((a, b) => (a.d > b.d ? 1 : -1));
-  return prio;
-}
-
-function find2_(quad, best, x, y, z) {
-  if (distanceFromQuadSquared(x, y) > Math.pow(0.5, z) * best.distSquared)
-    return;
-  if (z > 0) {
-    const prio = getCandidates(quad, x, y);
-    for (let i = 0; i < prio.length; i++) {
-      const tile = prio[i];
-      find2_(tile.t, best, 2 * tile.x, 2 * tile.y, z - 1);
-    }
-    return;
-  }
-  if (!quad.value) return;
-  const distSquared = (x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5);
-  //  console.log(quad.value + ": " + Math.sqrt(distSquared));
-  if (distSquared < best.distSquared) {
-    best.distSquared = distSquared;
-    best.value = quad.value;
-  }
-}
-
-function find(tree, x, y, z) {
-  if (z === 0) return { [z]: tree.value };
+function innerFind(tree, x, y, z, currentZ, acc) {
+  if (z === currentZ) return accumulateHits(acc, tree, currentZ);
   const leaf = getChild(tree, x, y);
-  if (!leaf) return { [z]: tree.value };
-  const dv = find(leaf, 2 * (x % 0.5), 2 * (y % 0.5), z - 1);
-  return Object.assign({}, dv, { [z]: tree.value });
+  if (leaf) innerFind(leaf, 2 * (x % 0.5), 2 * (y % 0.5), z, currentZ + 1, acc);
+  return accumulateHits(acc, tree, currentZ);
+}
+
+function find(tree, x, y, z, currentZ = 0) {
+  const acc = { x, y, z };
+  innerFind(tree, x, y, z, 0, acc);
+  return acc;
 }
 
 const quadBound = {
@@ -142,4 +94,4 @@ function add(tree, bounds, z, value) {
   create(tree, bounds, z, value);
 }
 
-module.exports = { add, find, find2 };
+module.exports = { add, find };
---
+++ quadtree.query.test.js
@@ -0,0 +1,32 @@
+const quadtree = require("./quadtree");
+
+test("zoom 0 quarter", () => {
+  const tree = { area: 0.25, value: 0.25 };
+  const expected = '{"0":{"area":0.0625,"value":1},"x":0.2,"y":0.2,"z":42}';
+  query(tree, 0.2, 0.2, expected);
+});
+
+test("zoom 1 quarter", () => {
+  const tree = { nw: { area: 1, value: 1 } };
+  const expected = '{"1":{"area":1,"value":1},"x":0.2,"y":0.2,"z":42}';
+  query(tree, 0.2, 0.2, expected);
+});
+
+test("zoom 3 double quarter", () => {
+  const tree = {
+    nw: {
+      area: 1,
+      value: 1,
+      se: { se: { area: 0.6399999999999997, value: 0.6399999999999997 } }
+    }
+  };
+  const expected =
+    '{"1":{"area":1,"value":1},"3":{"area":0.4095999999999996,"value":1},"x":0.4,"y":0.4,"z":42}';
+
+  query(tree, 0.4, 0.4, expected);
+});
+
+function query(tree, x, y, expected) {
+  const actual = quadtree.find(tree, x, y, 42);
+  expect(JSON.stringify(actual)).toBe(expected);
+}
