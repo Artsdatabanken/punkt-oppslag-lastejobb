dc09bbc631269d0778aaad5a9239ec6299bc3d19
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-07-30 21:41:13 +0200

    Don't average

--- quadtree/pyramid.js
+++ quadtree/pyramid.js
@@ -14,7 +14,7 @@ function build(tree, config) {
   const acc = items.reduce(
     (acc, e) => {
       if (config.mode === "class") {
-        if (e.p > tree.p) {
+        if (e.p > (tree.p || 0)) {
           tree.p = e.p;
           tree.v = e.v;
         }
@@ -28,10 +28,12 @@ function build(tree, config) {
     },
     { sum: 0, p: 0 }
   );
-  if (!tree.v) {
-    tree.v = ~~(0.25 * acc.sum);
+  if (config.mode !== "class") {
+    if (!tree.v) {
+      tree.v = ~~(0.25 * acc.sum);
+    }
+    tree.p = (tree.p || 0) + 0.25 * acc.p;
   }
-  tree.p = (tree.p || 0) + 0.25 * acc.p;
 }
 
 module.exports = {
