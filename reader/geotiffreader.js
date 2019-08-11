54e2571df1c0807a70d5a52e967ffd24524cf0bb
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-11 14:41:18 +0200

    Display geotiff resolution

--- reader/geotiffreader.js
+++ reader/geotiffreader.js
@@ -16,6 +16,7 @@ async function buildQuadTileset(layer, tree) {
   if (rasters.length !== 1)
     throw new Error("Can only handle GeoTiff containing single raster.");
   log.info("Indexing " + width + "x" + height + " raster");
+  log.info("Image resolution: " + width / (bbox[2] - bbox[0]) + " meters");
   index(rasters[0], tree, bbox, width, height, layer);
 }
 
