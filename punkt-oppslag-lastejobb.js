ca166cc4a44ea8e36e5a5d1b2e5d827a0c99c47d
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-11 10:51:00 +0200

    Move geotiff read to separate file

--- punkt-oppslag-lastejobb.js
+++ punkt-oppslag-lastejobb.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 const lastejobb = require("lastejobb");
 const { log } = require("lastejobb");
-const GeoTIFF = require("geotiff");
 const quadtree = require("./quadtree");
-const geometry = require("./geometry");
 const filesystemwriter = require("./writer/filesystemwriter");
 const Mbtileswriter = require("./writer/mbtileswriter");
 const fs = require("fs");
 const path = require("path");
 const pkg = require("./package");
+const geotiffreader = require("./reader/geotiffreader");
 
 log.info(pkg.name + " v" + pkg.version);
 if (process.argv.length !== 4)
@@ -59,7 +58,7 @@ async function processDataset(layer, tree) {
       " meters"
   );
   log.info("Reading:           " + layer.source);
-  await processTiff(layer, tree);
+  await geotiffreader.buildQuadTileset(layer, tree);
   // .then(x => {
   //      const coords = geometry.normalize([954000, 7940000, 0, 0], tree.bounds);
   if (layer.buildPyramid) {
@@ -87,60 +86,4 @@ async function processDataset(layer, tree) {
   //      filesystemwriter.write(tree, buildPath, layer);
   const mbtileswriter = new Mbtileswriter(buildPath);
   mbtileswriter.writeAll(tree, layer);
-}
-
-async function processTiff(layer, tree) {
-  const gt = await GeoTIFF.fromFile(layer.mapFile);
-  const imageCount = await gt.getImageCount();
-  if (imageCount !== 1)
-    throw new Error("Can only handle GeoTiff containing single image.");
-  const image = await gt.getImage(0);
-  const bbox = image.getBoundingBox();
-  const width = image.getWidth();
-  const height = image.getHeight();
-  const rasters = await image.readRasters();
-  if (rasters.length !== 1)
-    throw new Error("Can only handle GeoTiff containing single raster.");
-  log.info("Indexing " + width + "x" + height + " raster");
-  index(rasters[0], tree, bbox, width, height, layer);
-}
-
-function erNullverdi(value, nullverdier) {
-  if (Array.isArray(nullverdier)) {
-    for (let i = 0; i < nullverdier.length; i++)
-      if (value === nullverdier[i]) return true;
-  } else if (value === nullverdier) return true;
-  return false;
-}
-
-function index(raster, tree, bbox, width, height, layer) {
-  for (var y = 0; y < height; y++)
-    for (var x = 0; x < width; x++) {
-      const offset = y * width + x;
-      const value = raster[offset];
-      if (erNullverdi(value, layer.nullverdi)) continue;
-      const qvalue = quantize(layer.intervall, value);
-      if (Math.round(qvalue) > layer.intervall.normalisertVerdi[1])
-        throw new Error("Value out of range.  In:" + value + " Out:" + qvalue);
-      const coords = pixelToWorldCoordinates(bbox, x, y, width, height);
-      const xy = geometry.normalize(coords, tree.bounds);
-      const cursor = { bounds: xy, zoom: 0, targetZoom: layer.zoom };
-      quadtree.add(tree, cursor, layer, value);
-    }
-}
-
-function quantize(intervall, value) {
-  return (
-    ((value - intervall.original[0]) / intervall.original.bredde) *
-      intervall.normalisertVerdi.bredde +
-    intervall.normalisertVerdi[0]
-  );
-}
-
-function pixelToWorldCoordinates(bbox, x, y, width, height) {
-  const metersPerPixelX = (bbox[2] - bbox[0]) / width;
-  const metersPerPixelY = (bbox[3] - bbox[1]) / height;
-  const coX = bbox[0] + x * metersPerPixelX;
-  const coY = bbox[3] - y * metersPerPixelY;
-  return [coX, coY - metersPerPixelY, coX + metersPerPixelX, coY];
 }
---
+++ reader/geotiffreader.js
@@ -0,0 +1,62 @@
+const { log } = require("lastejobb");
+const GeoTIFF = require("geotiff");
+const geometry = require("../geometry");
+const quadtree = require("../quadtree");
+
+async function buildQuadTileset(layer, tree) {
+  const gt = await GeoTIFF.fromFile(layer.mapFile);
+  const imageCount = await gt.getImageCount();
+  if (imageCount !== 1)
+    throw new Error("Can only handle GeoTiff containing single image.");
+  const image = await gt.getImage(0);
+  const bbox = image.getBoundingBox();
+  const width = image.getWidth();
+  const height = image.getHeight();
+  const rasters = await image.readRasters();
+  if (rasters.length !== 1)
+    throw new Error("Can only handle GeoTiff containing single raster.");
+  log.info("Indexing " + width + "x" + height + " raster");
+  index(rasters[0], tree, bbox, width, height, layer);
+}
+
+function erNullverdi(value, nullverdier) {
+  if (Array.isArray(nullverdier)) {
+    for (let i = 0; i < nullverdier.length; i++)
+      if (value === nullverdier[i]) return true;
+  } else if (value === nullverdier) return true;
+  return false;
+}
+
+function index(raster, tree, bbox, width, height, layer) {
+  for (var y = 0; y < height; y++)
+    for (var x = 0; x < width; x++) {
+      const offset = y * width + x;
+      const value = raster[offset];
+      if (erNullverdi(value, layer.nullverdi)) continue;
+      const qvalue = quantize(layer.intervall, value);
+      if (Math.round(qvalue) > layer.intervall.normalisertVerdi[1])
+        throw new Error("Value out of range.  In:" + value + " Out:" + qvalue);
+      const coords = pixelToWorldCoordinates(bbox, x, y, width, height);
+      const xy = geometry.normalize(coords, tree.bounds);
+      const cursor = { bounds: xy, zoom: 0, targetZoom: layer.zoom };
+      quadtree.add(tree, cursor, layer, value);
+    }
+}
+
+function quantize(intervall, value) {
+  return (
+    ((value - intervall.original[0]) / intervall.original.bredde) *
+      intervall.normalisertVerdi.bredde +
+    intervall.normalisertVerdi[0]
+  );
+}
+
+function pixelToWorldCoordinates(bbox, x, y, width, height) {
+  const metersPerPixelX = (bbox[2] - bbox[0]) / width;
+  const metersPerPixelY = (bbox[3] - bbox[1]) / height;
+  const coX = bbox[0] + x * metersPerPixelX;
+  const coY = bbox[3] - y * metersPerPixelY;
+  return [coX, coY - metersPerPixelY, coX + metersPerPixelX, coY];
+}
+
+module.exports = { buildQuadTileset };
